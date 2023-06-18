ae1f271f92c03285659b0ff0345d46c0f0e07890
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-18 08:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,15 @@
 const screensize = document.getElementById("sizeInfo");
 const width = sizeInfo.querySelector("h1:first-child");
 const height = sizeInfo.querySelector("h1:last-child");
+// screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+// document.node.appendChild(screensize);
 
 function setScreenSize() {
 width.innerText = `Width: ${window.innerWidth}px`;
 height.innerText = `Height: ${window.innerHeight}px`;
 }
-
-
 window.addEventListener('resize', setScreenSize);
 setScreenSize();
-
 
 
 
