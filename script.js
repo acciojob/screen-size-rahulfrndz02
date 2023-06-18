efe7b5e0e0397b546d0e4e096b3ef087fd5fec3a
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-18 08:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //your JS code here. If required.
 
-function getWindowSize(){
+function setScreenSize(){
 	var weidth = document.documentElement.clientWidth;
 	var height = document.documentElement.clientHeight;
-	document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + width +  "and" + " Height: " + height + "</h1>";
+	document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + width + " Height: " + height + "</h1>";
 	
 }
+
+window.addEventListener('resize', setScreenSize);
+setScreenSize();
 
 
 
